669e3d8b4d6e17fb7e1f8543369ed304da0556ea
chemagonz <chemagonzby@gmail.com>
2024-04-29 13:35:00 +0200

    Insertado todos los Dao restantes de la base de datos

---
+++ app/src/main/java/com/advantys/brsmovilidaderp/Data/DataBase/Daos/Promobas_Dao.kt
@@ -0,0 +1,7 @@
+package com.advantys.brsmovilidaderp.Data.DataBase.Daos
+
+import com.advantys.brsmovilidaderp.Utils.BDUtil
+import javax.inject.Inject
+
+class Promobas_Dao @Inject constructor(private val databaseManager: BDUtil) {
+}
