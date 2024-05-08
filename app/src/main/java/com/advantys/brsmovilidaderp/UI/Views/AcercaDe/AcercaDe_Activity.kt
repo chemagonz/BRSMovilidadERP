d57cd156930308e9350ed6a490040a6c0bc85da8
chemagonz <chemagonzby@gmail.com>
2024-05-08 16:30:18 +0200

    -APARTADO AcercaDe_Activity FORMATEADO

--- app/src/main/java/com/advantys/brsmovilidaderp/UI/Views/AcercaDe/AcercaDe_Activity.kt
+++ app/src/main/java/com/advantys/brsmovilidaderp/UI/Views/AcercaDe/AcercaDe_Activity.kt
@@ -10,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import com.advantys.brsmovilidaderp.R
 import com.advantys.brsmovilidaderp.UI.ViewModels.Configuracion_ViewModel
+import com.advantys.brsmovilidaderp.Utils.actionBar
 import com.advantys.brsmovilidaderp.Utils.obtenerVersionApp
 import com.advantys.brsmovilidaderp.databinding.ActivityAcercaDeBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -21,6 +22,7 @@ import java.io.InputStreamReader
 
 @AndroidEntryPoint
 class AcercaDe_Activity : AppCompatActivity() {
+
     private lateinit var binding: ActivityAcercaDeBinding
     val configViewmodel : Configuracion_ViewModel by viewModels()
 
@@ -30,27 +32,27 @@ class AcercaDe_Activity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        actionBar("ACERCA DE")
 
-        supportActionBar?.apply {
-            setDisplayHomeAsUpEnabled(true)
-            setHomeButtonEnabled(true)
-            title = "ACERCA DE"
-        }
-
-        configViewmodel.getTerminal()
-        configViewmodel.terminal.observe(this, Observer { terminal ->
-           binding.acercadeTerminal.text = "Terminal nº: " +  terminal.toString()
-        })
-
+        mostrarTerminal()
         obtenerVersion()
         obtenerModelo()
         mostrarNovedades()
-
     }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun mostrarTerminal() {
+        configViewmodel.getTerminal()
+        configViewmodel.terminal.observe(this, Observer { terminal ->
+            binding.acercadeTerminal.text = "Terminal nº: " +  terminal.toString()
+        })
+    }
+
     private fun obtenerModelo() {
-        val modelo = getDeviceName()
+        val modelo = mostrarNombreDispositivo()
         binding.acercadeInfoterminal.setText(modelo)
     }
+
     private fun obtenerVersion() {
         binding.acercadeNombreVersion.text = "AdvantysERP v." + obtenerVersionApp()
     }
@@ -63,9 +65,11 @@ class AcercaDe_Activity : AppCompatActivity() {
             val bufferedReader = BufferedReader(InputStreamReader(inputStream))
             val stringBuilder = StringBuilder()
             var line: String?
+
             while (bufferedReader.readLine().also { line = it } != null) {
                 stringBuilder.append(line).append("\n")
             }
+
             inputStream.close()
             binding.acercadeWebView.loadDataWithBaseURL(null, stringBuilder.toString(), "text/html", "utf-8", null)
 
@@ -75,7 +79,7 @@ class AcercaDe_Activity : AppCompatActivity() {
     }
 
     //Devuelve el nombre del dispositivo que se está usando. Se inserta la función capitalize
-    fun getDeviceName(): String {
+    fun mostrarNombreDispositivo(): String {
         val manufacturer = Build.MANUFACTURER
         val model = Build.MODEL
         return if (model.startsWith(manufacturer)) capitalize(model) else capitalize(manufacturer) + " " + model
@@ -84,6 +88,7 @@ class AcercaDe_Activity : AppCompatActivity() {
     //Función para poner la primera letra en mayúsculas
     private fun capitalize(s: String?): String {
         if (s == null || s.length == 0) return ""
+
         val first = s[0]
         return if (Character.isUpperCase(first)) s else first.uppercaseChar()
             .toString() + s.substring(1)
